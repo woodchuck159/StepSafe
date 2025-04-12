895f72c3e989e27c35242809d5f74f8f7d182327
Ivan Dovbush <idovbush@purdue.edu>
2025-04-12 17:39:43 -0400

    documentation complete for functions.js file

--- WebApp/js/functions.js
+++ WebApp/js/functions.js
@@ -1,8 +1,10 @@
+// create a function to get the current location of the person by 'piggybacking' off of the API of a Purdue webite
 function getLocation() {
   mapRequest = new MapRequest();
   mapRequest.getMapLocation();
 }
 
+// create function to set desired final destination through voice
 function startVoiceNavigation() {
   const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   if (!SpeechRecognition) {
@@ -11,7 +13,7 @@ function startVoiceNavigation() {
   }
 
   const recognition = new SpeechRecognition();
-  recognition.lang = "en-US";
+  recognition.lang = "en-US"; // set default language to english
   recognition.interimResults = false;
   recognition.continuous = false; // listens only once
   recognition.maxAlternatives = 1;
@@ -23,6 +25,7 @@ function startVoiceNavigation() {
       console.log("Voice recognition started. Please speak...");
   };
 
+  // perform required actions upon hearing input voice
   recognition.onresult = function (event) {
       const spokenText = event.results[0][0].transcript;
       console.log("You said:", spokenText);
@@ -36,16 +39,17 @@ function startVoiceNavigation() {
       recognition.start(); // Restart listening
   };
 
-  // Improve error handling
+  // Improve error handling, display correct messages if error occurs when fetching geocodes
   recognition.onerror = function (event) {
       console.error("Speech recognition error:", event.error);
       alert("Error recognizing speech: " + event.error);
   };
 }
 
+// fetch longitude and latitude coordinates of detination name / address
 function fetchDestinationCoordinates(destinationName) {
   // Example fallback: default proximity near Purdue
-  const defaultProximity = '-86.9132,40.4237';
+  const defaultProximity = '-86.9132,40.4237'; // set default proximity to current location
 
   navigator.geolocation.getCurrentPosition((position) => {
     const lng = position.coords.longitude;
@@ -57,19 +61,23 @@ function fetchDestinationCoordinates(destinationName) {
   });
 }
 
+// create a function that will fetch the geocode of the user spoken location and set it as the desired location
 function fetchGeocode(destinationName, proximity) {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(destinationName)}.json?access_token=${mapboxgl.accessToken}&proximity=${proximity}&types=poi&autocomplete=true`;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(destinationName)}.json?access_token=${mapboxgl.accessToken}&proximity=${proximity}&autocomplete=true`;
 
+  // fetch the api database and connect the given address with the latitude and longitude coordinates
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
+      // set longitude and latitude coordinates as final destination if location is found
       if (data.features && data.features.length > 0) {
         const [lng, lat] = data.features[0].center;
-        directions.setDestination([lng, lat]);
+        directions.setDestination([lng, lat]); 
       } else {
         alert("Could not find the destination.");
       }
     })
+    // display proper message if error occurs in fetching user spoken destination
     .catch((err) => {
       console.error("Error fetching destination:", err);
       alert("There was a problem finding that place.");
