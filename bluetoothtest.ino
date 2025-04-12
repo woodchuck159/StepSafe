7dea5e8b3702ea218c95b01fcebde125898053e1
Mahdi El Husseini <135082877+MahdiHu06@users.noreply.github.com>
2025-04-12 14:37:38 -0700

    Add files via upload

--- bluetoothtest.ino
+++ bluetoothtest.ino
@@ -4,41 +4,63 @@ SoftwareSerial BTSerial(10, 11); // RXD, TXD
 const int trig = 5;
 const int echo = 6;
 
-long duration;
-int distance;
+// functions
+void checkWDT();
+void runSensor(const int trig, const int echo);
+void runBT();
 
-void setup(){
-  // check if a watchdog reset occurred
-  if (MCUSR & _BV(WDRF)) {
-  // Watchdog reset occurred
-  MCUSR &= ~_BV(WDRF); // Clear the flag
-  }
-  
+// setup the program
+void setup(){  
+  checkWDT(); // enable watchdog and check for reset
+  // set trigger inputs
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
-  Serial.begin(9600);
-  wdt_enable(WDTO_4S);
+  
+  Serial.begin(9600); // begine
   Serial.println("Starting up!\n");
+
+  // set up bluetooth
   BTSerial.begin(9600);
   Serial.println("Bluetooth ready\n");
 }
 
 void loop(){
-  wdt_reset();
-  digitalWrite(trig, LOW);
-  delayMicroseconds(12);
+  wdt_reset(); // reset watchdog timer to prevent unneccesary resetting
+  runSensor(trig, echo);  // run the sensor
+  runBT();
+
+  // set delay before starting again
+  delay(2000);
+}
+
+// setup watchdog timer and check for previous reset
+void checkWDT(){
+  // check if reset has occurred
+  if (MCUSR & _BV(WDRF)) {
+  // Watchdog reset occurred
+  MCUSR &= ~_BV(WDRF); // Clear the flag
+  }
+  // enable watchdog
+  wdt_enable(WDTO_4S);
+  
+}
+
+// run the sensor
+void runSensor(const int trig, const int echo){
+  digitalWrite(trig, LOW);  // trig is low
+  delayMicroseconds(12);  // minimum delay for arduino is 10 microseconds.
   digitalWrite(trig, HIGH);
   delayMicroseconds(12);
   digitalWrite(trig, LOW);
-  duration = pulseIn(echo, HIGH, 30000);
-  distance = duration * .017;
+
+  // duration and distance
+  long duration = pulseIn(echo, HIGH, 30000); // find duration until pulse is received
+  int distance = duration * .017; // calculate distance
   Serial.print("Distance: ");
   Serial.println(distance);
-if(BTSerial.available()){
-  Serial.write(BTSerial.read());
 }
-if(Serial.available()){
-  BTSerial.write(Serial.read());
-}
-delay(2000);
+
+void runBT(){
+  if(BTSerial.available()){Serial.write(BTSerial.read());}
+  if(Serial.available()){BTSerial.write(Serial.read());}
 }
