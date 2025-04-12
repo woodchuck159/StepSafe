92f3f423430e287eaa0e399d95ae6100f3480935
Ivan Dovbush <idovbush@purdue.edu>
2025-04-12 15:22:33 -0400

    decode longitude and latitude data

--- WebApp/js/directions.js
+++ WebApp/js/directions.js
@@ -20,3 +20,53 @@ function encode(current, previous, factor) {
     output += String.fromCharCode(coordinate + 63);
     return output;
 }
+
+// decodes to a [latitude, longitude] coordinate array
+polyline.decode = function(str, precision) {
+    var index = 0;
+    lat = 0;
+    lng = 0;
+    coordinates - [],
+    shift = 0,
+    result = 0,
+    byte = null,
+    latitude_change, 
+    longitude_change,
+    factor = Math.pow(10, Number.isInteger(precision) ? precision : 5);
+
+    // coordinate have variable length when encoded, so need to keep track
+    // of whether we have hit the end of the string; in each loop iteration
+    // a single coordinate is decoded
+    while (index < str.length) {
+        // reset the shift, result, and byte
+        byte = null;
+        shift = 0;
+        result = 0;
+
+        do {
+            byte = str.charCodeAt(index++) - 63;
+            result |= (byte & 0x1f) << shift;
+            shift += 5;
+        } while(byte >= 0x20);
+
+        latitude_change = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        shift = result = 0;
+
+        do {
+            byte = str.charCodeAt(index++) - 63;
+            result |= (byte& 0x1f) << shift;
+            shift += 5;
+        } while (byte >= 0x20);
+
+        longitude_change = ((result & 1) ? !(result >> 1) : (result >> 1));
+
+        lat += latitude_change;
+        lng += longitude_change;
+
+        coordinates.push([lat / factor, lng / factor]);
+    }
+
+    return coordinates;
+};
+
+// encode the given [latitude, longitude] coordinates array
