53be11ff74343caa6b32cb95812e69242ec51861
Mahdi El Husseini <135082877+MahdiHu06@users.noreply.github.com>
2025-04-13 14:49:20 -0400

    corrected Bluetooth and sensors

--- bluetoothtest.ino
+++ bluetoothtest.ino
@@ -1,9 +1,9 @@
 #include <SoftwareSerial.h>
 #include <avr/wdt.h>
 
-SoftwareSerial BTSerial(10, 11); // RXD, TXD
-const int trig = 5;
-const int echo = 6;
+SoftwareSerial BTSerial(10, 11); // TXD, RXD
+const int trig = 6;
+const int echo = 5;
 
 // functions
 void checkWDT();
@@ -13,11 +13,13 @@ void runBT();
 // setup the program
 void setup(){  
   checkWDT(); // enable watchdog and check for reset
-  // set trigger inputs
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
+  // set triggers
+  pinMode(trig, OUTPUT);  // output pin
+  pinMode(echo, INPUT); // input pin
+  pinMode(A0, OUTPUT);  // output pin
   
-  Serial.begin(9600); // begine
+  //start program
+  Serial.begin(9600); // begin
   Serial.println("Starting up!\n");
 
   // set up bluetooth
@@ -25,13 +27,14 @@ void setup(){
   Serial.println("Bluetooth ready\n");
 }
 
+// what needs to be run consistently
 void loop(){
   wdt_reset(); // reset watchdog timer to prevent unneccesary resetting
   runSensor(trig, echo);  // run the sensor
   runBT();
 
   // set delay before starting again
-  delay(2000);
+  delay(500);
 }
 
 // setup watchdog timer and check for previous reset
@@ -42,12 +45,12 @@ void checkWDT(){
   MCUSR &= ~_BV(WDRF); // Clear the flag
   }
   // enable watchdog
-  wdt_enable(WDTO_4S);
-  
+  wdt_enable(WDTO_1S);
 }
 
 // run the sensor
 void runSensor(const int trig, const int echo){
+  // write to digital pins
   digitalWrite(trig, LOW);  // trig is low
   delayMicroseconds(12);  // minimum delay for arduino is 10 microseconds.
   digitalWrite(trig, HIGH);
@@ -57,11 +60,18 @@ void runSensor(const int trig, const int echo){
   // duration and distance
   long duration = pulseIn(echo, HIGH, 30000); // find duration until pulse is received
   int distance = duration * .017; // calculate distance
+
+  // turn on speaker/buzzer if something is too close
+  if(distance <= 120){digitalWrite(A0, true);}else{digitalWrite(A0, false);}
+  
+  // print results
   Serial.print("Distance: ");
   Serial.println(distance);
+  // BTSerial.println(distance); // print in bluetooth terminal to test connection
 }
 
 void runBT(){
-  if(BTSerial.available()){Serial.write(BTSerial.read());}
-  if(Serial.available()){BTSerial.write(Serial.read());}
+  // print bluetooth input to arduino and arduino input to bluetooth
+  if(BTSerial.available()){Serial.print(BTSerial.readString());}
+  if(Serial.available()){BTSerial.print(Serial.readString());}
 }
